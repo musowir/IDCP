483eb929c0c2ccc440f6bc03208f04443b41b774
Abdu Musowir U <musowir@pg.cusat.ac.in>
2022-09-08 12:41:56 +0530

    given department isstaff status

--- department/views.py
+++ department/views.py
@@ -30,6 +30,7 @@ def register(request):
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
+            user.is_staff=True
             user.save()
             profile = profile_form.save(commit=False)
             profile.user = user

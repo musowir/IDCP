1a188a96202103598c9d336babba2e608c5e979e
Abdu Musowir U <musowir@pg.cusat.ac.in>
2022-10-22 08:55:55 +0530

    course more info modal added

--- static/login.js
+++ static/login.js
@@ -1,11 +1,68 @@
-function viewhide() {
+function depshow() {
     var d = document.getElementById("dep");
-    var s = document.getElementById("stu")
-    if (d.style.display === "none") {
-      d.style.display = "block";
-      s.style.display ="none";
-    } else {
-        s.style.display = "block";
-        d.style.display ="none";
-    }
-  }
+    var s = document.getElementById("stu");
+    var f = document.getElementById('fac');
+    var st = document.getElementById('staf');
+    var a = document.getElementById('adm');
+    d.style.display = "block";
+    s.style.display = "none";
+    f.style.display = "none";
+    st.style.display = "none";
+    a.style.display = "none";
+}
+
+function stushow() {
+  var d = document.getElementById("dep");
+  var s = document.getElementById("stu");
+  var f = document.getElementById('fac');
+  var st = document.getElementById('staf');
+  var a = document.getElementById('adm');
+  d.style.display = "none";
+  s.style.display = "block";
+  f.style.display = "none";
+  st.style.display = "none";
+  a.style.display = "none";
+}
+
+function staffshow() {
+  var d = document.getElementById("dep");
+  var s = document.getElementById("stu");
+  var f = document.getElementById('fac');
+  var st = document.getElementById('staf');
+  var a = document.getElementById('adm');
+  
+  d.style.display = "none";
+  s.style.display = "none";
+  f.style.display = "none";
+  st.style.display = "block";
+  a.style.display = "none";
+}
+
+function facshow() {
+  var d = document.getElementById("dep");
+  var s = document.getElementById("stu");
+  var f = document.getElementById('fac');
+  var st = document.getElementById('staf');
+  var a = document.getElementById('adm');
+
+  d.style.display = "none";
+  s.style.display = "none";
+  f.style.display = "block";
+  st.style.display = "none";
+  a.style.display = "none";
+}
+
+function admshow() {
+  var d = document.getElementById("dep");
+  var s = document.getElementById("stu");
+  var f = document.getElementById('fac');
+  var st = document.getElementById('staf');
+  var a = document.getElementById('adm');
+  
+  d.style.display = "none";
+  s.style.display = "none";
+  f.style.display = "none";
+  st.style.display = "none";
+  a.style.display = "block";
+
+}
